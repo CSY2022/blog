2edf319bf9fcad5a68a90f4a0d0e14afafab5a76
CSY2022 <tel19807034568@outlook.com>
2025-03-16 17:39:34 +0800

    Update some configs

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/06/Markdown 基本语法/","2024/06/02/每一天日落🌇，与美好相遇/"];function toRandomPost(){
+var posts=["2024/Markdown 基本语法/","2024/每一天日落🌇，与美好相遇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
