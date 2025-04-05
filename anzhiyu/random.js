a086289a743f91872561201a3b64d1e02c992bd5
CSY2022 <tel19807034568@outlook.com>
2025-04-05 22:09:09 +0800

    Remove Qing ming as a mourn day

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/Markdown语法指南/","2025/每一天日落，与美好相遇/","2025/仲卿何罪有,世路本多艰/"];function toRandomPost(){
+var posts=["2025/Markdown语法指南/","2025/每一天日落，与美好相遇/","2025/仲卿何罪有，世路本多艰/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
